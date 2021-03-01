ef8f9c8fc935e66e41ae0992ef28f7e959b6fc56
Kushagra Singh <kushagra.singhkushagra.singh@gmail.com>
2021-03-01 12:41:15 +0530

    march

--- registration.js
+++ registration.js
@@ -4,3 +4,12 @@ function show(){
   function hide() {
     document.getElementById("box").style.display = "none";
   }
+
+function selectOther(val){
+  var element=document.getElementById('other');
+  if(val=='other'){
+    element.style.display='block';
+  }else{
+    element.style.display = 'none';
+  }
+}
